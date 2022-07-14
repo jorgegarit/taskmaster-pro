7b3f4b6fb194f7374d1bc1195d00fe8e07e4f880
Jorge Garit <jorgegarit@gmail.com>
2022-07-13 22:39:55 -0400

    added the datepicker to the due date edit option on taks

--- assets/js/script.js
+++ assets/js/script.js
@@ -108,12 +108,21 @@ $(".list-group").on("click", "span", function() {
   // swap out elements
   $(this).replaceWith(dateInput);
 
+  // enable jquery ui datepicker
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function() {
+      // when calendar is closed, force a "change" event on the 'dateInput'
+      $(this).trigger("change");
+    }
+  });
+
   // automatically focus on new elements
   dateInput.trigger("focus");
 });
 
 // value of duedate was changed
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   // get current text
   var date = $(this)
     .val()
